d7db4697d0d9a5a3a3cad076db49778a4bf31f92
wayne <wayne50102@gmail.com>
2018-01-05 10:42:29 +0800

    v2 加上圖片

--- app/src/main/java/com/demo/demoproject2/MainActivity.java
+++ app/src/main/java/com/demo/demoproject2/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.demoproject2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
     }
 }
